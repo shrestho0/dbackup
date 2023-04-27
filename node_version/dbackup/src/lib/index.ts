41f1a689427bf41b84f8faae85d900b366548253
Sadril Amin Shrestho <sadrilaminshrestho@gmail.com>
2023-04-27 15:12:42 +0600

    some stuff changed

---
+++ node_version/dbackup/src/lib/index.ts
@@ -0,0 +1,6 @@
+const MY_NAME = "Shrestho"
+import { list_all_files } from "./object_storage/object-list-files"
+export {
+    MY_NAME,
+    list_all_files
+}
---
+++ node_version/dbackup/src/routes/+page.svelte
@@ -0,0 +1,7 @@
+<script>
+	import { list_all_files } from '$lib';
+</script>
+
+{#await list_all_files() then files}
+	{files}
+{/await}
